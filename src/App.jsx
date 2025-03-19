256cd7ba529e65b6dd08c06ec728cf22d6691838
Arifata <arifetamulugeta@gmail.com>
2025-03-20 01:18:35 +0300

    creating portfolio website of mine

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello this is from App.jsx component</h1>
+      <button className='btn'>hello</button>
+    </div>
+  )
+}
+
+export default App
