ec6ca0a759a6cd37c87ac2243de94b89d2a76e4a
Arifata <arifetamulugeta@gmail.com>
2025-03-23 16:53:36 +0300

    Expreince component completed

--- src/Component/Header/HeaderSocials.jsx
+++ src/Component/Header/HeaderSocials.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from "react";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 const HeaderSocials = () => {
   return (
-    <div className='header__socials'>
-        <a href="https://linkedin.com" target='_blank'><FaLinkedin /></a>
-        <a href="https://github.com/Arifeta" target='_blank'><FaGithub /></a>
-        <a href="https://facebook.com" target='_blank'><FaFacebook /></a>
+    <div className="header__socials">
+      <a href="https://linkedin.com" target="_blank">
+        <FaLinkedin />
+      </a>
+      <a href="https://github.com/Arifeta" target="_blank">
+        <FaGithub />
+      </a>
+      <a href="https://facebook.com" target="_blank">
+        <FaFacebook />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
